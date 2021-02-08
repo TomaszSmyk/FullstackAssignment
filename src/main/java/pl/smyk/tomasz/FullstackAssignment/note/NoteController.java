89a04fa092543dbc89406667e95b51da3483e0e9
Tomasz Smyk <tomasz.smyk99@wp.pl>
2021-02-08 15:41:21 +0100

    Add NoteController and NoteService

--- src/main/java/pl/smyk/tomasz/FullstackAssignment/note/NoteController.java
+++ src/main/java/pl/smyk/tomasz/FullstackAssignment/note/NoteController.java
@@ -1,4 +1,30 @@
 package pl.smyk.tomasz.FullstackAssignment.note;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
 public class NoteController {
+
+    @Autowired
+    private NoteService noteService;
+
+    @RequestMapping("/notes")
+    public List<Note> getAllNotes() {
+        log.info("Getting all notes");
+        return noteService.getNotes();
+    }
+
+    @RequestMapping("/notes/{id}")
+    public Note getNote(@PathVariable String id) {
+        log.info("Getting note with id = " + id);
+        return noteService.getNoteById(id);
+    }
 }
--- src/main/java/pl/smyk/tomasz/FullstackAssignment/note/NoteService.java
+++ src/main/java/pl/smyk/tomasz/FullstackAssignment/note/NoteService.java
@@ -10,9 +10,17 @@ import java.util.List;
 @Service
 public class NoteService {
     private List<Note> notes = Arrays.asList(
-            new Note("Title 1", "Content 1"),
-            new Note("Title 2", "Content 2"),
-            new Note("Title 3", "Content 3"),
-            new Note("Title 4", "Content 4")
+            new Note("1","Title 1", "Content 1"),
+            new Note("2","Title 2", "Content 2"),
+            new Note("3","Title 3", "Content 3"),
+            new Note("4","Title 4", "Content 4")
     );
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public Note getNoteById(String id) {
+        return notes.stream().filter(x -> x.getId().equals(id)).findFirst().get();
+    }
 }
